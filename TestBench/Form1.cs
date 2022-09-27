60da66ad307bbe0c7f352bb51872137aee7b9c94
Jon Kristensen <jon@jonkr.dk>
2022-09-27 12:40:16 +0200

    Read hourly temperatures in denmark for the last 10 years, and store result in csv-file.

--- TestBench/Form1.cs
+++ TestBench/Form1.cs
@@ -106,7 +106,7 @@ namespace TestBench
             foreach (Station station in stations)
             {
                 if ((stationList.ContainsKey(station.StationId) == false) &&
-                    (station.ParameterId.Contains("temp_dry") == true) &&
+                    (station.ParameterId.Contains("temp_mean_past1h") == true) &&
                     (station.Country == "DNK"))
                 {
                     stationList.Add(station.StationId, station);
@@ -116,37 +116,131 @@ namespace TestBench
             }
             Debug.WriteLine($"Added {stationList.Count} stations");
 
+            int stationCount = 0;
+
+            DateTime startTime = new DateTime(2012, 1, 1, 0, 0, 0, 0, DateTimeKind.Local);
+            DateTime endTime = new DateTime(2021, 12, 31, 23, 59, 59, 0, DateTimeKind.Local);
+
             foreach (Station station in stationList.Values)
             {
                 int count = 0;
                 int limit = 10000;
                 int offset = 0;
 
-                do
+                // if (station.Name == "Mejrup")
                 {
-                    var observations = await client.GetObservations(
-                        limit: limit,
-                        offset: offset,
-                        stationId: station.StationId,
-                        fromDateTime: new DateTime(2001, 1, 1, 0, 0, 0, DateTimeKind.Local),
-                        toDateTime: new DateTime(2022, 1, 1, 0, 0, 0, DateTimeKind.Local),
-                        parameterId: "temp_dry");
 
-                    count = observations.ContainsKey(station.StationId) ? observations[station.StationId].Count : 0;
-                    offset += count;
-
-                    Debug.WriteLine($"Got {count} elements for {station.StationId}");
-                    if (count > 0)
+                    do
                     {
-                        Debug.WriteLine($"First timestamp is : {observations.First().Value.Parameters.First().Value.ObservationData.First().Value.Observed.ToString()}");
-                        Debug.WriteLine($"Last timestamp is : {observations.First().Value.Parameters.First().Value.ObservationData.Last().Value.Observed.ToString()}");
-                    }
-                } while (count > 0);
+                        var observations = await client.GetObservations(
+                            limit: limit,
+                            offset: offset,
+                            stationId: station.StationId,
+                            fromDateTime: startTime,
+                            toDateTime: endTime,
+                            parameterId: "temp_mean_past1h");
 
+                        if (observations.ContainsKey(station.StationId))
+                        {
+                            StationData stationData = observations[station.StationId];
+                            count = stationData.Count;
+                            
+                            foreach (string parameterName in stationData.Parameters.Keys)
+                            {
+                                ParameterData parameterData;
+                                if (station.Parameters.ContainsKey(parameterName))
+                                {
+                                    parameterData = station.Parameters[parameterName];
+                                }
+                                else
+                                {
+                                    parameterData = new ParameterData()
+                                    {
+                                        Location = null,
+                                        Name = parameterName,
+                                        ObservationData = new SortedList<DateTime, Observation>()
+                                    };
+                                    station.Parameters.Add(parameterName, parameterData);
+                                }
 
+                                foreach (Observation observation in stationData.Parameters[parameterName].ObservationData.Values)
+                                {
+                                    parameterData.ObservationData.Add(observation.Observed, observation);
+                                }
+                            }
+
+                        }
+                        else
+                        {
+                            count = 0;
+                        }
+
+                        offset += count;
+
+                        Debug.WriteLine($"Got {count} elements for {station.Name}");
+                        if (count == 0)
+                        {
+                            // Shit Hack :-(
+                            stationCount++;
+                        }
+
+                        //if (count > 0)
+                        //{
+                        //    Debug.WriteLine($"First timestamp is : {observations.First().Value.Parameters.First().Value.ObservationData.First().Value.Observed.ToString()}");
+                        //    Debug.WriteLine($"Last timestamp is : {observations.First().Value.Parameters.First().Value.ObservationData.Last().Value.Observed.ToString()}");
+                        //}
+
+                    } while (count > 0);
+
+                    //if (stationCount >= 5)
+                    //{
+                    //    break;
+                    //}
+
+                }
                 // Only take the first station
-                break;
+                // break;
             }
+
+
+            StreamWriter streamWriter = new StreamWriter("C:\\Users\\Jon\\Desktop\\data.csv");
+
+            streamWriter.Write("Timestamp; Year");
+            foreach (Station station in stationList.Values)
+            {
+                if (station.Parameters.Count > 0)
+                {
+                    streamWriter.Write($";{station.Name}");
+                }
+            }
+
+            streamWriter.WriteLine("");
+
+            for (DateTime time = startTime; time <= endTime; time += TimeSpan.FromHours(1.0))
+            {
+                streamWriter.Write($"{time};{time.Year}");
+
+                foreach (Station station in stationList.Values)
+                {
+                    if (station.Parameters.Count > 0)
+                    {
+
+                        if (station.Parameters["temp_mean_past1h"].ObservationData.ContainsKey(time))
+                        {
+                            streamWriter.Write($";{station.Parameters["temp_mean_past1h"].ObservationData[time].Value}");
+                        }
+                        else
+                        {
+                            streamWriter.Write(";");
+                        }
+                    }
+                }
+                streamWriter.WriteLine("");
+            }
+
+            streamWriter.Close();
+
+            Debug.WriteLine("We are done");
         }
     }
 }
